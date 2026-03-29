01827f004eb5ca4e1ae2364dc90401af626a2327
psudhakarsudhap <psudhakarsudhap@gmail.com>
2026-03-29 19:01:54 +0530

    initial commit

---
+++ src/main/java/com/vvit/githubdockercicd/GithubDockerCicdApplication.java
@@ -0,0 +1,13 @@
+package com.vvit.githubdockercicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDockerCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubDockerCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vvit/githubdockercicd/GithubDockerCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvit.githubdockercicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDockerCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
