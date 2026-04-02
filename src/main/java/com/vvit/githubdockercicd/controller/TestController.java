d664b43038d05ba2e73f975ce9ac3f72804c2897
psudhakarsudhap <psudhakarsudhap@gmail.com>
2026-04-02 09:56:30 +0530

    new update

--- src/main/java/com/vvit/githubdockercicd/controller/TestController.java
+++ src/main/java/com/vvit/githubdockercicd/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World!";
+        return "Hello, World! update update";
     }
     //http://localhost:8080/api/v1/test/greet?name=John
     @GetMapping("/greet")
